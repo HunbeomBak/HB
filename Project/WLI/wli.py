9cf098255de53756b55a3183cb97ad31f17b554b
Hunbeom Bak <h2hb0307@gmail.com>
2019-10-30 21:22:59 +0900

    study wli

--- Project/WLI/wli.py
+++ Project/WLI/wli.py
@@ -1,6 +1,15 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 import nidaqmx
+
+def Init():
+    a = []
+    b = []
+    c = []
+    d = []
+
+    return a,b,c,d
 
 def Cam_setting(cam):
     cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -11,14 +20,8 @@ def Cam_setting(cam):
     cam.set(cv2.CAP_PROP_GAIN, 10)
 
 
-def Get_cam(camera):
-    rec, frame = camera.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    return frame
-
-def Move():
+def Move(voltage):
     pass
 
 
@@ -26,28 +29,74 @@ def OPD():
     pass
 
 
+#카메라 지정
+camera = cv2.VideoCapture('./data/20190911_4 max.avi')
+n_frame = camera.get(cv2.CAP_PROP_FRAME_COUNT)
+w,h = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)), int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-camera = cv2.VideoCapture(0)
+center = (int(h/2),int(w/2))
+print(center)
 
-Cam_setting(camera)
 
+#카메라 설정
+#Cam_setting(camera)
 
-image_list = []
+# 이미지 넣을 리스트 만듦
+Gray_list, B_list ,G_list ,R_list  = Init()
+Gray_I, B_I,G_I,R_I = Init()
 
-for i in range(4):
+A =[]
+a = range(int(n_frame))
 
-    gray = Get_cam(camera)
+#사진 20번찍으면서 각각 리스트에 넣음
+for i in a:
+    rec, frame = camera.read()
 
-    image_list.append(gray)
+    B,G,R = frame[:,:,0], frame[:,:,1], frame[:,:,2]
+    B_list.append(B)
+    B_I.append(B[center])
 
-    Move()
-    OPD()
+    G_list.append(G)
+    G_I.append(G[center])
 
-    print(i)
+    R_list.append(R)
+    R_I.append(R[center])
 
-print(len(image_list))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    Gray_list.append(gray)
 
-cv2.imshow('test',image_list[3])
-cv2.waitKey(0)
+    cv2.imshow('test', gray)
+    #cv2.waitKey(10)
+
+    print(i)  # 몇번째 루프인지 출력
+
+    #Move() #PZT 움직일 함수
+    #OPD() # OPD 전처리
 
 camera.release()
+
+print("촬영 종료")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line, = ax.plot(a, B_I, 'b') # 5 points tolerance
+line, = ax.plot(a, B_I, 'o', picker=5)  # 5 points tolerance
+
+
+
+def onpick(event):
+    global B_I, A
+
+    if event.artist!=line: return True
+
+    N = len(event.ind)
+    if not N: return True
+
+    i = event.ind[0]
+    A = [B_I[i],B_I[i+1],B_I[i+2],B_I[i+3],B_I[i+4]]
+    print(A)
+
+fig.canvas.mpl_connect('pick_event', onpick)
+
+
+plt.show()
